d322f240e3bb45861da6dba96a25574a625ac003
DownToTwo <yourf1493@gmail.com>
2023-12-11 00:38:00 +0200

    add comments to vector.h

--- vector.h
+++ vector.h
@@ -1,8 +1,7 @@
-/* This file was automatically generated.  Do not edit! By autoheaders */
 #ifndef _VECTOR_
 #define _VECTOR_
-#include <stdbool.h>
-#include <stddef.h>
+#include <stdbool.h> //for bool type
+#include <stddef.h>  //for size_t
 typedef int element;
 #define VECTOR_FOREACH(vec, i)                                                 \
   for (Iterator i = begin((vec)), en = end((vec)); (i) != en; next(&(i)))
@@ -14,15 +13,25 @@ typedef struct vector {
 } Vector;
 #define GROWFACTOR (2)
 typedef element *Iterator;
+// Remove element at index from vector
 void removeAt(Vector *vector, size_t index);
+// Return and remove last element
 element popback(Vector *vector);
+// Insert element at the end of the vector
 void pushback(Vector *vector, element elem);
-void set(Vector *vector, size_t idx, element number);
+// Set element at index in vector
+void set(Vector *vector, size_t idx, element elem);
+// Get element from vector at index with bounds checking
 element at(Vector *vector, size_t idx);
+// Free up memory for vector
 void DynamicArrayDectructor(Vector *vector);
+// Resize vector by GROWFACTOR
 void resize(Vector *vector);
+// Allocate memory for `capacity` elements
 Vector DynamicArray(size_t capacity);
+// Check whether vector is valid
 bool isVectorValid(const Vector *vec);
+// Dummy Iterator implementation
 Iterator end(const Vector *iter);
 void next(Iterator *iter);
 Iterator begin(const Vector *iter);
